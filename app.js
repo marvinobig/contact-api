e82026b8a8439b41de2497d78be7c10d8a0b5242
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-09-30 10:37:42 +0100

    add 404 handler

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const nodemailer = require("nodemailer");
 const app = express();
+
+//email endpoint
+app.post("api/email", (req, res, next) => {
+  try {
+  } catch (err) {
+    next(err);
+  }
+});
+
+// error handler
+app.all("*", (req, res) => {
+  res.status(404).send("Route Not Found");
+});
+
+app.use((err, req, res) => {});
 
 module.exports = app;
