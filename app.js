d618feea05c353c4d8fa8981a132e3ae3ff13a4f
marvinobig <88102362+marvinobig@users.noreply.github.com>
2022-09-30 12:45:54 +0100

    remove comment

--- app.js
+++ app.js
@@ -15,9 +15,6 @@ app.post("/api/contact", (req, res, next) => {
     const { sender, subject, name, message } = req.body;
 
     const transporter = nodemailer.createTransport({
-      //   host: process.env.HOST,
-      //   port: process.env.PORT,
-      //   secure: true,
       service: "gmail",
       auth: {
         user: process.env.EMAIL_ADDR,
