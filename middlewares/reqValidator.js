f4201c750efa7d6b92fd1d548c4e01f0e969ce85
Marvin <88102362+marvinobig@users.noreply.github.com>
2024-07-31 09:09:33 +0100

    simplify reqValidator.js regex

--- middlewares/reqValidator.js
+++ middlewares/reqValidator.js
@@ -11,15 +11,15 @@ const reqValidator = (req, res, next) => {
     ? (validationObj.from = "field needs a valid email address")
     : (validationObj.from = undefined);
 
-  !name.match(/^\w{2,}$/m)
+  !name.match(/[A-Za-z]{2,}/g)
     ? (validationObj.name = "field needs your name")
     : (validationObj.name = undefined);
 
-  !subject.match(/[A-Za-z0-9]{2,}/)
+  !subject.match(/[A-Za-z0-9]{2,}/g)
     ? (validationObj.subject = "field needs to include 2 characters or more")
     : (validationObj.subject = undefined);
 
-  !message.match(/([A-Za-z0-9\W]{5,})+/m)
+  !message.match(/[A-Za-z0-9\s]{5,}/g)
     ? (validationObj.message = "field needs to include a sentence")
     : (validationObj.message = undefined);
 
